d2ad61331c6434e803a68e0bbdcc776adb8a8e19
codepanther <pezechigozirim@gmail.com>
2022-05-30 23:58:32 +0100

    show single task endpoint

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -5,29 +5,39 @@ const index = async (req, res) => {
     const tasks = await Task.find({});
     res.status(200).json({ tasks });
   } catch (error) {
-    res.status(500).json({ msg: error })
+    res.status(500).json({ msg: error });
   }
 };
 
-const show = (req, res) => {
-  res.json({ id: req.params.id })
+const show = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findById(taskId);
+
+    if (!task)
+      return res.status(404).json({ msg: `No task with id: ${taskId}` });
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const create = async (req, res) => {
   try {
-    const task = await Task.create(req.body)
-    res.status(201).json({ task })
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
   } catch (error) {
-    res.status(500).json({ msg: error })
+    res.status(500).json({ msg: error });
   }
 };
 
 const update = (req, res) => {
-  res.send('Update task')
+  res.send('Update task');
 };
 
 const destroy = (req, res) => {
-  res.send('Delete task')
+  res.send('Delete task');
 };
 
 module.exports = { index, show, create, update, destroy };
