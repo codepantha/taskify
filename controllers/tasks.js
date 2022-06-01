f8412b170fe0fd005c2a9d03cf4c5d105ae345a4
codepanther <pezechigozirim@gmail.com>
2022-06-01 13:04:11 +0100

    remove redundant checks as it's already being handled by asyncWrapper and my errorHandler middleware

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -10,11 +10,6 @@ const show = asyncWrapper(async (req, res, next) => {
   const { id: taskId } = req.params;
   const task = await Task.findById(taskId);
 
-  if (!task) {
-    const error = new Error(`Task not found`)
-    error.status = 404;
-    return next(error)
-  }
   res.status(200).json({ task });
 });
 
@@ -33,20 +28,12 @@ const update = asyncWrapper(async (req, res) => {
     { returnDocument: 'after', runValidators: true }
   );
 
-  if (!task)
-    return res
-      .status(404)
-      .json({ msg: 'Error! Cannot update non-existent task.' });
-
   res.status(200).json({ task });
 });
 
 const destroy = asyncWrapper(async (req, res) => {
   const { id } = req.params;
   const task = await Task.deleteOne({ _id: id });
-
-  if (!task)
-    return res.status(404).json({ msg: `Task with id: ${id} not found ` });
 
   res.status(204).send();
 });
